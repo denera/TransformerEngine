2ed1c5db90c741828eab331f5b2312b86f85ccc0
Alp Dener <adener@nvidia.com>
2024-05-08 18:06:53 +0000

    added granular UB config to UB example problem
    Signed-off-by: Alp Dener <adener@nvidia.com>

--- examples/pytorch/comm_gemm_overlap/test_ln_mlp.py
+++ examples/pytorch/comm_gemm_overlap/test_ln_mlp.py
@@ -39,34 +39,53 @@ def train(args):
                             rank=LOCAL_RANK,
                             world_size=WORLD_SIZE)
     tp_group = dist.new_group(backend="nccl")
+    tp_size = dist.get_world_size(tp_group)
 
-    # Intialize userbuffers
     torch.cuda.set_device(LOCAL_RANK)
     torch.manual_seed(args.seed+LOCAL_RANK)
     torch.cuda.manual_seed(args.seed+LOCAL_RANK)
+
+    # Intialize userbuffers
+    ag_cfg = {  # Ring-exchange All-Gather overlap for fc1_fprop and fc2_dgrad
+        'method': 'ring_exchange',
+        'num_splits' : 8,
+        'num_sm' : 1,
+        'set_sm_margin' : False,
+    }
+    rs_cfg = {  # Reduce-scatter overlap for fc1_dgrad and fc2_fprop
+        'num_splits' : 4,
+        'num_sm' : 1,
+        'set_sm_margin' : True,
+    }
+    hidden_size = args.num_heads * args.head_dim
     if args.comm_overlap:
         te.module.base.initialize_ub(
-            [args.seq_length * args.batch_size, args.hidden_size],
-            WORLD_SIZE,
+            [args.seq_length * args.batch_size, hidden_size],
+            tp_size,
             use_fp8 = args.fp8,
-            dtype = args.dtype
+            dtype = args.dtype,
+            ub_cfgs = {
+                'fc1_fprop': ag_cfg,
+                'fc1_dgrad': rs_cfg,
+                'fc2_fprop': rs_cfg,
+                'fc2_dgrad': ag_cfg,
+            },
         )
 
     # Initialize TE model
     ln_mlp = te.LayerNormMLP(
-        args.hidden_size, 3 * args.hidden_size,
+        hidden_size, 3 * hidden_size,
         params_dtype = args.dtype,
         device = 'cuda',
         tp_group = tp_group,
-        tp_size = WORLD_SIZE,
+        tp_size = tp_size,
         set_parallel_mode = True,
         sequence_parallel = True,
-        ub_bulk_wgrad = args.comm_overlap,
-        ub_bulk_dgrad = args.comm_overlap,
-        # ub_overlap_rs_dgrad = args.comm_overlap,
+        micro_batch_size = args.batch_size,
+        ub_overlap_rs_dgrad = args.comm_overlap,
         ub_overlap_rs = args.comm_overlap,
         ub_overlap_ag = args.comm_overlap,
-    ).cuda()
+    )
 
     # Fp8 recipe setup
     fp8_format = Format.HYBRID
@@ -81,7 +100,7 @@ def train(args):
         dist_print(f"Iter {i}", all_ranks=args.verbose)
 
         dist_print("|-- Generate random input batch", all_ranks=args.verbose)
-        x = torch.rand((args.seq_length * args.batch_size // WORLD_SIZE, args.hidden_size),
+        x = torch.rand((args.seq_length * args.batch_size // tp_size, hidden_size),
                        dtype=args.dtype, device='cuda')
 
         dist_print("|-- Forward pass", all_ranks=args.verbose)
@@ -101,12 +120,14 @@ if __name__ == "__main__":
                                                  "GEMM+comm overlap via Userbuffers.")
     parser.add_argument('-i', "--num-iters", type=int, default=5,
                         help="Number of dummy 'training' iterations.")
-    parser.add_argument('-b', "--batch-size", type=int, default=4,
+    parser.add_argument('-b', "--batch-size", type=int, default=2,
                         help="Input batch size.")
-    parser.add_argument('-s', "--seq-length", type=int, default=512,
+    parser.add_argument('-s', "--seq-length", type=int, default=2048,
                         help="Input sequence length.")
-    parser.add_argument('-n', "--hidden-size", type=int, default=1024,
+    parser.add_argument('-n', "--num-heads", type=int, default=64,
                         help="Number of attention heads.")
+    parser.add_argument('-d', "--head-dim", type=int, default=128,
+                        help="Dimension of each attention head.")
     parser.add_argument("--seed", type=int, default=1234,
                         help="RNG seed.")
     parser.add_argument("--fp8", action="store_true", default=False,
