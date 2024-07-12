f3de2dce66f7edb75883b4791b41f07c88541cf3
Leopold Cambier <lcambier@nvidia.com>
2024-07-12 16:29:49 -0700

    Scripts

--- tests/pytorch/distributed/run.sh
+++ tests/pytorch/distributed/run.sh
@@ -3,62 +3,34 @@
 set -eux
 set -o pipefail
 
-export NVSHMEM_DISABLE_CUDA_VMM=1 
-export NVSHMEM_DISABLE_NCCL=1 
-export NVSHMEM_REMOTE_TRANSPORT=none 
-#export LD_LIBRARY_PATH=/workdir/TransformerEngine/:/workdir/libnvshmem_2.11.0-5+cuda12.0_x86_64/lib:$LD_LIBRARY_PATH 
-export LD_LIBRARY_PATH=/workdir/TransformerEngine/:/workdir/libnvshmem_3.0.12-0+cuda12.3_x86_64/lib:$LD_LIBRARY_PATH 
+export CUDA_MODULE_LOADING=EAGER
+export NVSHMEM_DISABLE_CUDA_VMM=1
+export NVSHMEM_DISABLE_NCCL=1
+export NVSHMEM_REMOTE_TRANSPORT=none
+export LD_LIBRARY_PATH=/workdir/:/workdir/libnvshmem_3.0.12-0+cuda12.3_x86_64/lib:$LD_LIBRARY_PATH
 
-# NVTE_NVSHMEM_WAIT=0 torchrun --nproc-per-node=8 tests/pytorch/distributed/run_gemm_with_overlap.py --check-numerics -b 2 -s 2048 -n 64 -d 128 --p2p --comm-type ag --warmup-iters 5 --timing-iters 10 --backend nvshmem
-# NVTE_NVSHMEM_WAIT=0 torchrun --nproc-per-node=8 tests/pytorch/distributed/run_gemm_with_overlap.py --check-numerics -b 2 -s 2048 -n 96 -d 128 --p2p --comm-type ag --warmup-iters 5 --timing-iters 10 --backend nvshmem
-# NVTE_NVSHMEM_WAIT=0 torchrun --nproc-per-node=8 tests/pytorch/distributed/run_gemm_with_overlap.py --check-numerics -b 2 -s 4096 -n 64 -d 128 --p2p --comm-type ag --warmup-iters 5 --timing-iters 10 --backend nvshmem
-# NVTE_NVSHMEM_WAIT=0 torchrun --nproc-per-node=8 tests/pytorch/distributed/run_gemm_with_overlap.py --check-numerics -b 2 -s 4096 -n 96 -d 128 --p2p --comm-type ag --warmup-iters 5 --timing-iters 10 --backend nvshmem
-# NVTE_NVSHMEM_WAIT=0 torchrun --nproc-per-node=8 tests/pytorch/distributed/run_gemm_with_overlap.py --check-numerics -b 2 -s 8192 -n 64 -d 128 --p2p --comm-type ag --warmup-iters 5 --timing-iters 10 --backend nvshmem
-# NVTE_NVSHMEM_WAIT=0 torchrun --nproc-per-node=8 tests/pytorch/distributed/run_gemm_with_overlap.py --check-numerics -b 2 -s 8192 -n 96 -d 128 --p2p --comm-type ag --warmup-iters 5 --timing-iters 10 --backend nvshmem
-# NVTE_NVSHMEM_WAIT=0 torchrun --nproc-per-node=8 tests/pytorch/distributed/run_gemm_with_overlap.py --check-numerics -b 2 -s 2048 -n 64 -d 128 --p2p --comm-type rs --warmup-iters 5 --timing-iters 10 --backend nvshmem
-# NVTE_NVSHMEM_WAIT=0 torchrun --nproc-per-node=8 tests/pytorch/distributed/run_gemm_with_overlap.py --check-numerics -b 2 -s 2048 -n 96 -d 128 --p2p --comm-type rs --warmup-iters 5 --timing-iters 10 --backend nvshmem
-# NVTE_NVSHMEM_WAIT=0 torchrun --nproc-per-node=8 tests/pytorch/distributed/run_gemm_with_overlap.py --check-numerics -b 2 -s 4096 -n 64 -d 128 --p2p --comm-type rs --warmup-iters 5 --timing-iters 10 --backend nvshmem
-# NVTE_NVSHMEM_WAIT=0 torchrun --nproc-per-node=8 tests/pytorch/distributed/run_gemm_with_overlap.py --check-numerics -b 2 -s 4096 -n 96 -d 128 --p2p --comm-type rs --warmup-iters 5 --timing-iters 10 --backend nvshmem
-# NVTE_NVSHMEM_WAIT=0 torchrun --nproc-per-node=8 tests/pytorch/distributed/run_gemm_with_overlap.py --check-numerics -b 2 -s 8192 -n 64 -d 128 --p2p --comm-type rs --warmup-iters 5 --timing-iters 10 --backend nvshmem
-# NVTE_NVSHMEM_WAIT=0 torchrun --nproc-per-node=8 tests/pytorch/distributed/run_gemm_with_overlap.py --check-numerics -b 2 -s 8192 -n 96 -d 128 --p2p --comm-type rs --warmup-iters 5 --timing-iters 10 --backend nvshmem
-# 
-# NVTE_NVSHMEM_WAIT=1 torchrun --nproc-per-node=8 tests/pytorch/distributed/run_gemm_with_overlap.py --check-numerics -b 2 -s 2048 -n 64 -d 128 --p2p --comm-type ag --warmup-iters 5 --timing-iters 10 --backend nvshmem
-# NVTE_NVSHMEM_WAIT=1 torchrun --nproc-per-node=8 tests/pytorch/distributed/run_gemm_with_overlap.py --check-numerics -b 2 -s 2048 -n 96 -d 128 --p2p --comm-type ag --warmup-iters 5 --timing-iters 10 --backend nvshmem
-# NVTE_NVSHMEM_WAIT=1 torchrun --nproc-per-node=8 tests/pytorch/distributed/run_gemm_with_overlap.py --check-numerics -b 2 -s 4096 -n 64 -d 128 --p2p --comm-type ag --warmup-iters 5 --timing-iters 10 --backend nvshmem
-# NVTE_NVSHMEM_WAIT=1 torchrun --nproc-per-node=8 tests/pytorch/distributed/run_gemm_with_overlap.py --check-numerics -b 2 -s 4096 -n 96 -d 128 --p2p --comm-type ag --warmup-iters 5 --timing-iters 10 --backend nvshmem
-# NVTE_NVSHMEM_WAIT=1 torchrun --nproc-per-node=8 tests/pytorch/distributed/run_gemm_with_overlap.py --check-numerics -b 2 -s 8192 -n 64 -d 128 --p2p --comm-type ag --warmup-iters 5 --timing-iters 10 --backend nvshmem
-# NVTE_NVSHMEM_WAIT=1 torchrun --nproc-per-node=8 tests/pytorch/distributed/run_gemm_with_overlap.py --check-numerics -b 2 -s 8192 -n 96 -d 128 --p2p --comm-type ag --warmup-iters 5 --timing-iters 10 --backend nvshmem
-# NVTE_NVSHMEM_WAIT=1 torchrun --nproc-per-node=8 tests/pytorch/distributed/run_gemm_with_overlap.py --check-numerics -b 2 -s 2048 -n 64 -d 128 --p2p --comm-type rs --warmup-iters 5 --timing-iters 10 --backend nvshmem
-# NVTE_NVSHMEM_WAIT=1 torchrun --nproc-per-node=8 tests/pytorch/distributed/run_gemm_with_overlap.py --check-numerics -b 2 -s 2048 -n 96 -d 128 --p2p --comm-type rs --warmup-iters 5 --timing-iters 10 --backend nvshmem
-# NVTE_NVSHMEM_WAIT=1 torchrun --nproc-per-node=8 tests/pytorch/distributed/run_gemm_with_overlap.py --check-numerics -b 2 -s 4096 -n 64 -d 128 --p2p --comm-type rs --warmup-iters 5 --timing-iters 10 --backend nvshmem
-# NVTE_NVSHMEM_WAIT=1 torchrun --nproc-per-node=8 tests/pytorch/distributed/run_gemm_with_overlap.py --check-numerics -b 2 -s 4096 -n 96 -d 128 --p2p --comm-type rs --warmup-iters 5 --timing-iters 10 --backend nvshmem
-# NVTE_NVSHMEM_WAIT=1 torchrun --nproc-per-node=8 tests/pytorch/distributed/run_gemm_with_overlap.py --check-numerics -b 2 -s 8192 -n 64 -d 128 --p2p --comm-type rs --warmup-iters 5 --timing-iters 10 --backend nvshmem
-# NVTE_NVSHMEM_WAIT=1 torchrun --nproc-per-node=8 tests/pytorch/distributed/run_gemm_with_overlap.py --check-numerics -b 2 -s 8192 -n 96 -d 128 --p2p --comm-type rs --warmup-iters 5 --timing-iters 10 --backend nvshmem
+for s in 2048 4096 8192
+do
+	for n in 64 96
+	do 
+		for backend in nvshmem user_buffers
+		do
 
-torchrun --nproc-per-node=8 tests/pytorch/distributed/run_gemm_with_overlap.py --check-numerics -b 2 -s 2048 -n 64 -d 128 --p2p --comm-type ag --warmup-iters 5 --timing-iters 10 --backend nvshmem
-torchrun --nproc-per-node=8 tests/pytorch/distributed/run_gemm_with_overlap.py --check-numerics -b 2 -s 2048 -n 96 -d 128 --p2p --comm-type ag --warmup-iters 5 --timing-iters 10 --backend nvshmem
-torchrun --nproc-per-node=8 tests/pytorch/distributed/run_gemm_with_overlap.py --check-numerics -b 2 -s 4096 -n 64 -d 128 --p2p --comm-type ag --warmup-iters 5 --timing-iters 10 --backend nvshmem
-torchrun --nproc-per-node=8 tests/pytorch/distributed/run_gemm_with_overlap.py --check-numerics -b 2 -s 4096 -n 96 -d 128 --p2p --comm-type ag --warmup-iters 5 --timing-iters 10 --backend nvshmem
-torchrun --nproc-per-node=8 tests/pytorch/distributed/run_gemm_with_overlap.py --check-numerics -b 2 -s 8192 -n 64 -d 128 --p2p --comm-type ag --warmup-iters 5 --timing-iters 10 --backend nvshmem
-torchrun --nproc-per-node=8 tests/pytorch/distributed/run_gemm_with_overlap.py --check-numerics -b 2 -s 8192 -n 96 -d 128 --p2p --comm-type ag --warmup-iters 5 --timing-iters 10 --backend nvshmem
-torchrun --nproc-per-node=8 tests/pytorch/distributed/run_gemm_with_overlap.py --check-numerics -b 2 -s 2048 -n 64 -d 128 --p2p --comm-type rs --warmup-iters 5 --timing-iters 10 --backend nvshmem
-torchrun --nproc-per-node=8 tests/pytorch/distributed/run_gemm_with_overlap.py --check-numerics -b 2 -s 2048 -n 96 -d 128 --p2p --comm-type rs --warmup-iters 5 --timing-iters 10 --backend nvshmem
-torchrun --nproc-per-node=8 tests/pytorch/distributed/run_gemm_with_overlap.py --check-numerics -b 2 -s 4096 -n 64 -d 128 --p2p --comm-type rs --warmup-iters 5 --timing-iters 10 --backend nvshmem
-torchrun --nproc-per-node=8 tests/pytorch/distributed/run_gemm_with_overlap.py --check-numerics -b 2 -s 4096 -n 96 -d 128 --p2p --comm-type rs --warmup-iters 5 --timing-iters 10 --backend nvshmem
-torchrun --nproc-per-node=8 tests/pytorch/distributed/run_gemm_with_overlap.py --check-numerics -b 2 -s 8192 -n 64 -d 128 --p2p --comm-type rs --warmup-iters 5 --timing-iters 10 --backend nvshmem
-torchrun --nproc-per-node=8 tests/pytorch/distributed/run_gemm_with_overlap.py --check-numerics -b 2 -s 8192 -n 96 -d 128 --p2p --comm-type rs --warmup-iters 5 --timing-iters 10 --backend nvshmem
+			echo "b 2 s ${s} n ${n} d 128 p2p ag bf16 split ${backend}"
+			torchrun --nproc-per-node=8 tests/pytorch/distributed/run_gemm_with_overlap.py --check-numerics -b 2 -s ${s} -n ${n} -d 128 --p2p --comm-type ag                --warmup-iters 5 --timing-iters 10 --backend ${backend}
+			
+			echo "b 2 s ${s} n ${n} d 128 p2p rs bf16 split ${backend}"
+			torchrun --nproc-per-node=8 tests/pytorch/distributed/run_gemm_with_overlap.py --check-numerics -b 2 -s ${s} -n ${n} -d 128 --p2p --comm-type rs                --warmup-iters 5 --timing-iters 10 --backend ${backend}
+			
+			echo "b 2 s ${s} n ${n} d 128 p2p ag fp8 split ${backend}"
+			torchrun --nproc-per-node=8 tests/pytorch/distributed/run_gemm_with_overlap.py --check-numerics -b 2 -s ${s} -n ${n} -d 128 --p2p --comm-type ag --fp8          --warmup-iters 5 --timing-iters 10 --backend ${backend}
 
-torchrun --nproc-per-node=8 tests/pytorch/distributed/run_gemm_with_overlap.py --check-numerics -b 2 -s 2048 -n 64 -d 128 --p2p --comm-type ag --warmup-iters 5 --timing-iters 10 --backend user_buffers 
-torchrun --nproc-per-node=8 tests/pytorch/distributed/run_gemm_with_overlap.py --check-numerics -b 2 -s 2048 -n 96 -d 128 --p2p --comm-type ag --warmup-iters 5 --timing-iters 10 --backend user_buffers 
-torchrun --nproc-per-node=8 tests/pytorch/distributed/run_gemm_with_overlap.py --check-numerics -b 2 -s 4096 -n 64 -d 128 --p2p --comm-type ag --warmup-iters 5 --timing-iters 10 --backend user_buffers 
-torchrun --nproc-per-node=8 tests/pytorch/distributed/run_gemm_with_overlap.py --check-numerics -b 2 -s 4096 -n 96 -d 128 --p2p --comm-type ag --warmup-iters 5 --timing-iters 10 --backend user_buffers 
-torchrun --nproc-per-node=8 tests/pytorch/distributed/run_gemm_with_overlap.py --check-numerics -b 2 -s 8192 -n 64 -d 128 --p2p --comm-type ag --warmup-iters 5 --timing-iters 10 --backend user_buffers 
-torchrun --nproc-per-node=8 tests/pytorch/distributed/run_gemm_with_overlap.py --check-numerics -b 2 -s 8192 -n 96 -d 128 --p2p --comm-type ag --warmup-iters 5 --timing-iters 10 --backend user_buffers 
-torchrun --nproc-per-node=8 tests/pytorch/distributed/run_gemm_with_overlap.py --check-numerics -b 2 -s 2048 -n 64 -d 128 --p2p --comm-type rs --warmup-iters 5 --timing-iters 10 --backend user_buffers 
-torchrun --nproc-per-node=8 tests/pytorch/distributed/run_gemm_with_overlap.py --check-numerics -b 2 -s 2048 -n 96 -d 128 --p2p --comm-type rs --warmup-iters 5 --timing-iters 10 --backend user_buffers 
-torchrun --nproc-per-node=8 tests/pytorch/distributed/run_gemm_with_overlap.py --check-numerics -b 2 -s 4096 -n 64 -d 128 --p2p --comm-type rs --warmup-iters 5 --timing-iters 10 --backend user_buffers 
-torchrun --nproc-per-node=8 tests/pytorch/distributed/run_gemm_with_overlap.py --check-numerics -b 2 -s 4096 -n 96 -d 128 --p2p --comm-type rs --warmup-iters 5 --timing-iters 10 --backend user_buffers 
-torchrun --nproc-per-node=8 tests/pytorch/distributed/run_gemm_with_overlap.py --check-numerics -b 2 -s 8192 -n 64 -d 128 --p2p --comm-type rs --warmup-iters 5 --timing-iters 10 --backend user_buffers 
-torchrun --nproc-per-node=8 tests/pytorch/distributed/run_gemm_with_overlap.py --check-numerics -b 2 -s 8192 -n 96 -d 128 --p2p --comm-type rs --warmup-iters 5 --timing-iters 10 --backend user_buffers 
-
+			echo "b 2 s ${s} n ${n} d 128 p2p rs fp8 split ${backend}"
+			torchrun --nproc-per-node=8 tests/pytorch/distributed/run_gemm_with_overlap.py --check-numerics -b 2 -s ${s} -n ${n} -d 128 --p2p --comm-type rs --fp8          --warmup-iters 5 --timing-iters 10 --backend ${backend}
+			
+			echo "b 2 s ${s} n ${n} d 128 p2p rs fp8 atomic ${backend}"
+			torchrun --nproc-per-node=8 tests/pytorch/distributed/run_gemm_with_overlap.py --check-numerics -b 2 -s ${s} -n ${n} -d 128 --p2p --comm-type rs --fp8 --atomic --warmup-iters 5 --timing-iters 10 --backend ${backend}
+		done
+	done
+done
 
